f0a31567e9f13783163aad931f8cdca48b5c9d87
kensuke-kamata <kn12kamata@gmail.com>
2025-06-22 15:17:38 +0900

    feat: basic window creation

--- Volante.cpp
+++ Volante.cpp
@@ -1,9 +1,75 @@
 ﻿#include "Volante.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-using namespace std;
+constexpr unsigned int SCR_WIDTH  = 800;
+constexpr unsigned int SCR_HEIGHT = 600;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Volante", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cerr << "failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+        std::cerr << "failed to initialize GLAD" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << "OpenGL Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
--- Volante.h
+++ Volante.h
@@ -1,4 +1,40 @@
 ﻿#pragma once
 
-#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
+namespace Volante {
+    using Vec2 = glm::vec2;
+    using Vec3 = glm::vec3;
+    using Vec4 = glm::vec4;
+    using Mat2 = glm::mat2;
+    using Mat3 = glm::mat3;
+    using Mat4 = glm::mat4;
+    using Quat = glm::quat;
+
+    constexpr float PI = glm::pi<float>();
+    constexpr float TWO_PI = 2.0f * PI;
+    constexpr float HALF_PI = PI / 2.0f;
+    constexpr float GRAVITY = 9.81f;
+
+    inline Vec3 cross(const Vec3& a, const Vec3& b) {
+        return glm::cross(a, b);
+    }
+
+    inline float dot(const Vec3& a, const Vec3& b) {
+        return glm::dot(a, b);
+    }
+
+    inline Vec3 normalize(const Vec3& v) {
+        return glm::normalize(v);
+    }
+
+    inline float length(const Vec3& v) {
+        return glm::length(v);
+    }
+
+    inline float distance(const Vec3& a, const Vec3& b) {
+        return glm::distance(a, b);
+    }
+}
